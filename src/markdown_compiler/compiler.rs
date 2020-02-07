9a1ade256c4e1d0ea6b4d4b9fcd10433a8393e43
Doug Rudolph <drudolph914@gmail.com>
2020-02-07 14:20:50 -0500

    removing struct variable current_char

--- src/markdown_compiler/compiler.rs
+++ src/markdown_compiler/compiler.rs
@@ -10,13 +10,13 @@ const H6: &str = &"######";
 
 #[derive(Debug)]
 struct Token <'a> {
-  class: Option<&'a str>,
+  group: Option<&'a str>,
   value: Option<&'a str>,
 }
 
 impl <'a> Token <'a>{
-  fn new(class: Option<&'a str>, value: Option<&'a str>) -> Token <'a> {
-    return Token { class, value };
+  fn new(group: Option<&'a str>, value: Option<&'a str>) -> Token <'a> {
+    return Token { group, value };
   }
 }
 
@@ -26,11 +26,16 @@ impl <'a> Token <'a>{
 struct Lexer {
   text: String,
   position: i32,
-  current_char: Option<char>,
 }
 
 impl Lexer {
   fn new(text: String, position: i32, current_char: Option<char>) -> Lexer {
-    return Lexer {text, position, current_char};
+    return Lexer {text, position };
   }
+
+  fn parse() {
+
+  }
+
 }
+
